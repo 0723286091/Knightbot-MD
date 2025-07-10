af0787b0966769a20c601a1591e3e08822ab15b9
0723286091 <gozy269@gmail.com>
2025-07-10 15:27:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Gozy', // Your name
+  botOwner: 'Ton.k', // Your name
   ownerNumber: '254794292800', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
